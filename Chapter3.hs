2bfbae9a3fa88f1e0f994c48a727134dfeb2ae11
meriy100 <ttattataa@gmail.com>
2017-04-20 15:54:35 +0900

    spec

--- Chapter3.hs
+++ Chapter3.hs
@@ -21,21 +21,23 @@ nAnd x y = True
 
 main :: IO ()
 main = hspec $ do
-  describe "myAnd" $ do
-    it "return Bool" $ do
-      (myAnd True True) `shouldBe` (True :: Bool)
-      (myAnd True False) `shouldBe` (False :: Bool)
-      (myAnd False True) `shouldBe` (False :: Bool)
-      (myAnd False False) `shouldBe` (False :: Bool)
-  describe "myOr" $ do
-    it "return Bool" $ do
-      (myOr True True) `shouldBe` (True :: Bool)
-      (myOr True False) `shouldBe` (True :: Bool)
-      (myOr False True) `shouldBe` (True :: Bool)
-      (myOr False False) `shouldBe` (False :: Bool)
-  describe "nAnd" $ do
-    it "return Bool" $ do
-      (nAnd True True) `shouldBe` (False :: Bool)
-      (nAnd True False) `shouldBe` (True :: Bool)
-      (nAnd False True) `shouldBe` (True :: Bool)
-      (nAnd False False) `shouldBe` (True :: Bool)
+  describe "3-4" $ do
+    context "myAnd" $ do
+      it "return Bool" $ do
+        (myAnd True True) `shouldBe` (True :: Bool)
+        (myAnd True False) `shouldBe` (False :: Bool)
+        (myAnd False True) `shouldBe` (False :: Bool)
+        (myAnd False False) `shouldBe` (False :: Bool)
+    context "myOr" $ do
+      it "return Bool" $ do
+        (myOr True True) `shouldBe` (True :: Bool)
+        (myOr True False) `shouldBe` (True :: Bool)
+        (myOr False True) `shouldBe` (True :: Bool)
+        (myOr False False) `shouldBe` (False :: Bool)
+  describe "3-5" $ do
+    context "nAnd" $ do
+      it "return Bool" $ do
+        (nAnd True True) `shouldBe` (False :: Bool)
+        (nAnd True False) `shouldBe` (True :: Bool)
+        (nAnd False True) `shouldBe` (True :: Bool)
+        (nAnd False False) `shouldBe` (True :: Bool)
