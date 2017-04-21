601a00cc3bbb887c973b9f8a8fbd172e416aa7bc
meriy100 <ttattataa@gmail.com>
2017-04-21 13:25:42 +0900

    3-9

--- Chapter3.hs
+++ Chapter3.hs
@@ -18,6 +18,12 @@ nAnd :: Bool -> Bool -> Bool
 nAnd True True = False
 nAnd x y = True
 
+-- 3-9
+threeDifferent :: Integer -> Integer -> Integer -> Bool
+threeDifferent x y z
+  | x == y = False
+  | x == z = False
+  | otherwise = y /= z
 
 main :: IO ()
 main = hspec $ do
@@ -41,3 +47,11 @@ main = hspec $ do
         (nAnd True False) `shouldBe` (True :: Bool)
         (nAnd False True) `shouldBe` (True :: Bool)
         (nAnd False False) `shouldBe` (True :: Bool)
+
+  describe "3-9" $ do
+    context "threeDifferent" $ do
+      it "return Bool" $ do
+        (threeDifferent 1 2 3) `shouldBe` (True :: Bool)
+        (threeDifferent 1 1 3) `shouldBe` (False :: Bool)
+        (threeDifferent 1 2 1) `shouldBe` (False :: Bool)
+        (threeDifferent 1 2 2) `shouldBe` (False :: Bool)
