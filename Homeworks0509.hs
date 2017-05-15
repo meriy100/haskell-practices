f8c47b881caf041e93036bae032d4814900323c0
meriy100 <ttattataa@gmail.com>
2017-05-15 16:58:52 +0900

    7-29

--- Homeworks0509.hs
+++ Homeworks0509.hs
@@ -12,11 +12,25 @@ joinLine (word:line)
   | otherwise = word
 joinLine _ =  ""
 
+--- 7-29
+joinLines :: Line -> String
+joinLines (word:line)
+  | (length line) > 0 = joinLines ( ( word ++ "\n" ++ (head line) ):(tail line) )
+  | otherwise = word
+joinLines _ =  ""
+
+
 main :: IO ()
 main = hspec $ do
   describe "7-28" $ do
     context "joinLine" $ do
       it "cat dog and bird" $ do
         (joinLine ["cat", "dog", "bird"]) `shouldBe` "cat dog bird"
+      it "empty" $ do
+        (joinLine []) `shouldBe` ""
+  describe "7-29" $ do
+    context "joinLines" $ do
+      it "cat dog and bird" $ do
+        (joinLines ["cat", "dog", "bird"]) `shouldBe` "cat\ndog\nbird"
       it "empty" $ do
         (joinLine []) `shouldBe` ""
